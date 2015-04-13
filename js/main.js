1482ba18d9aea39469a47390dde9a6b8c3c5f3c9
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-04-13 23:42:37 +0800

    debug of include threeJs and the main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var scene = new THREE.Scene(),
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000 ),
-	renderer = new THREE.WebGLRenderer();
+window.onload = function(argument) {
+	var scene = new THREE.Scene(),
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+		renderer = new THREE.WebGLRenderer();
 
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
+
+	// 
+
+}
