ba074e4ea94d7e060c38bc40ad9b0da649ef8fe1
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-04-13 23:55:19 +0800

    create function render, now we can see the cube

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function(argument) {
+	/* setting of scence, camera and renderer */
 	var scene = new THREE.Scene(),
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
 		renderer = new THREE.WebGLRenderer();
@@ -6,6 +7,19 @@ window.onload = function(argument) {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	// 
+	/* start to create the cube */
+	var geometry = new THREE.BoxGeometry(1, 1, 1),
+		material = new THREE.MeshBasicMaterial({
+			color: 0xff00fd
+		}),
+		cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
 
+	camera.position.z = 5;
+
+	function render () {
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	}
+	render();
 }
